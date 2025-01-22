7eea4b523c93d0daa112d0f16c10c4139806bf12
alexmc <alexandre.carvalho@gmail.com>
2025-01-22 10:22:20 +0000

    favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import '../styles/globals.css'
+import Meta from '../components/Meta'
 // import Header from '../components/Header'
 // import Footer from '../components/Footer'
 
 function MyApp({ Component, pageProps }) {
   return (
     <div className="min-h-screen flex flex-col">
+      <Meta />
       {/* <Header /> */}
       <main className="flex-grow">
         <Component {...pageProps} />
