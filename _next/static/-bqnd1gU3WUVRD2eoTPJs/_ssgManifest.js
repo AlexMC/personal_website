ed891420b05c9559a4bc46a178773a2e127e3094
AlexMC <alexandre.carvalho@gmail.com>
2025-11-24 16:49:44 +0000

    Deploying to gh-pages from @ AlexMC/personal_website@dc95401e35cf58b1d7bb97194e679f7e92a849ce 🚀

---
+++ _next/static/-bqnd1gU3WUVRD2eoTPJs/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002F[slashpage]","\u002Fblog","\u002Fblog\u002F[slug]","\u002Fslashes","\u002Fwork","\u002Fwork\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
